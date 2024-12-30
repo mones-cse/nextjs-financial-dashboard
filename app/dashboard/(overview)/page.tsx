0cc9df5f6cfac1fc92deb7ee412117c122e347bd
mones <mones.cse@gmail.com>
2024-12-30 12:29:21 +0600

    chapter-10 ppr

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,7 +3,7 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import CardWrapper from "@/app/ui/dashboard/cards";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchCardData } from "../lib/data";
+import { fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
 import {
   RevenueChartSkeleton,
