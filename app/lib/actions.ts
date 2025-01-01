bcd736be825e7434eddb112cfc75547f4cdcdac2
mones <mones.cse@gmail.com>
2025-01-01 21:11:35 +0600

    chapter 14 create-form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,9 +6,15 @@ import { redirect } from "next/navigation";
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer",
+  }),
+  amount: z.coerce.number().gt(0, {
+    message: "Please enter an amount greater than 0",
+  }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select a status",
+  }),
   date: z.string(),
 });
 
@@ -16,7 +22,16 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
   //   const rawFormData = {
   //     customerId: formData.get("customerId"),
   //     amount: formData.get("amount"),
@@ -24,11 +39,19 @@ export async function createInvoice(formData: FormData) {
   //   };
 
   //   const rawFormData = Object.fromEntries(formData.entries());
-  const { customerId, amount, status } = CreateInvoice.parse({
+
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
   try {
@@ -39,9 +62,9 @@ export async function createInvoice(formData: FormData) {
   } catch (error) {
     console.log("🚀 ~ createInvoice ~ error", error);
     // todo: handle the ts error because of the return type
-    // return {
-    //   message: "Database Error: Failed to Create Invoice.",
-    // };
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
   }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
